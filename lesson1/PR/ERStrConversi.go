8f4909b90422a130a53212a6828bbdbb7bb11f29
eserranda <eserranda@gmail.com>
2023-08-08 14:56:49 +0800

    Konversi Nilai Int to Str

--- lesson1/PR/ERStrConversi.go
+++ lesson1/PR/ERStrConversi.go
@@ -5,13 +5,28 @@ import (
 	"strconv"
 )
 
-func main() {
-	int := "1a23"
-	integers, err := strconv.Atoi(int)
-
+// konversi nilai String to Integers
+func StrToInt() {
+	str := "123"
+	// strconv.Atoi untuk mengkonversi string menjadi integer.
+	// Fungsi ini mengembalikan dua nilai, hasil konversi dan error (jika ada error).
+	result, err := strconv.Atoi(str)
+	//Memeriksa apakah ada error saat melakukan konversi nilai str.
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Hasil Konversi String To Integer :", result)
+}
 
-	fmt.Println(integers)
+// konversi nilai integer to string
+func IntToStr() {
+	int := 3043
+	// Mengkonversi nilai integer menjadi string
+	stringVersion := strconv.Itoa(int)
+	fmt.Println("Hasil Konversi Integer To String :", stringVersion)
+}
+
+func main() {
+	StrToInt()
+	IntToStr()
 }
--- lesson1/PR/ERkonfersi.go
+++ lesson1/PR/ERkonfersi.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var F = 30
-	var C float64 = (float64(F) - 32) * 5 / 9
+	var F float32 = 30
+	var C float32 = (float32(F) - 32) * 5 / 9
 
-	fmt.Printf("%.f Fahrenheit = %.2f Celsius", float64(F), C)
+	fmt.Printf("%.f Fahrenheit = %.2f Celsius", F, C)
 	// f adalah nilai yang digantikan adalah float atau bilangan pecahan.
 	// 2 angka desimal di belakang koma
 	// '%' = tanda bahwa di situ ada suatu nilai yang akan dimasukkan dan menggantikan tanda % tersebut saat mencetak.
