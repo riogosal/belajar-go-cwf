23e82dc52beaa73c5e40cdf95cf2d08865a7977c
eserranda <eserranda@gmail.com>
2023-08-17 03:50:55 +0800

    Write and Read data menggunakan Json

--- PR/eser/todo/newFilm.go
+++ PR/eser/todo/newFilm.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
@@ -18,22 +19,34 @@ type Film struct {
 	Genres            []Genre
 }
 
-func InputDataGenre(film *Film) {
-	for {
-		fmt.Print("Masukkan Genre atau Tekan (e) untuk exit: ")
-		var genreFilm Genre
-		// fmt.Println(film.Genres)
-		fmt.Scanln(&genreFilm)
-		if genreFilm == "e" {
-			break
-		} else {
-			film.Genres = append(film.Genres, genreFilm)
+func removeGenre(film *Film, hapusGenre Genre) {
+	for i, genre := range film.Genres {
+		fmt.Println(i)
+		if genre == hapusGenre {
+			film.Genres = append(film.Genres[:i], film.Genres[i+1:]...)
+			break // Keluar dari loop setelah elemen ditemukan dan dihapus
 		}
 	}
 }
 
+// func InputDataGenre(film *Film) {
+// 	for {
+// 		fmt.Print("Masukkan Genre atau Tekan (e) untuk exit: ")
+// 		var genreFilm Genre
+// 		// fmt.Println(film.Genres)
+// 		fmt.Scanln(&genreFilm)
+// 		if genreFilm == "e" {
+
+// 			break
+// 		} else {
+// 			film.Genres = append(film.Genres, genreFilm)
+// 		}
+// 	}
+// }
+
 func InputDataFilm(film *Film) {
 	// newData := Film{}
+
 	scanner := bufio.NewReader(os.Stdin)
 	fmt.Print("Masukkan Judul: ")
 	film.Judul, _ = scanner.ReadString('\n')
@@ -67,7 +80,7 @@ func InputDataFilm(film *Film) {
 	}
 	film.Durasi = newDurasi
 
-	InputDataGenre(film)
+	// InputDataGenre(film)
 }
 
 func UpdateNewFilm(film *Film) {
@@ -153,46 +166,65 @@ func DataFilm(film *Film) {
 	} else {
 		fmt.Println("Kamu belum memiliki daftar film Favorit, Silahkan Input Film Terlebih Dahulu")
 	}
-
 }
 
 func UpdateGenre(film *Film) {
-	var inputGenre int
-	fmt.Println("Silakan pilih nomor yang ingin diubah:")
-	for i, genre := range film.Genres {
-		length := i + 1                       // kita tambah 1 karena index [film.Genres] itu mulai dari 0
-		fmt.Printf("%d. %s\n", length, genre) //kita pakai %d karena length itu integer, kalo %s itu string
+	if len(film.Genres) > 0 {
+		var inputGenre int
+		fmt.Println("Silakan pilih nomor yang ingin diubah:")
+		for i, genre := range film.Genres {
+			length := i + 1                       // kita tambah 1 karena index [film.Genres] itu mulai dari 0
+			fmt.Printf("%d. %s\n", length, genre) //kita pakai %d karena length itu integer, kalo %s itu string
+		}
+		_, err := fmt.Scanln(&inputGenre) // kita parsing dalu data yang di input dari string ke integers
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		if inputGenre < 1 || inputGenre > len(film.Genres) { // Kita buat validasi, kalau user input 0 itu tidak bisa, ///karena index sekarang dihitung dari 1
+			fmt.Println("Nomor tidak valid") // kita hitung len (film.genders) lalu kita bandingkan dgn data yng di input, Intinya tidak boleh melebihi nilai Len
+			return
+		}
+		var newGenre Genre // ini untuk variabel untuk menampung nilai dari &newGenre
+		fmt.Print("Masukkan genre baru: ")
+		_, err = fmt.Scanln(&newGenre)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		inputGenre = inputGenre - 1 // kita kurangi 1 karena di atas di tambahkan 1, biar dia membaca index mulai dari 0 dst
+		film.Genres[inputGenre] = newGenre
+		fmt.Println("Genre berhasil diubah:", newGenre)
+	} else {
+		fmt.Println("Anda Belum menginput data genre!")
 	}
-
-	_, err := fmt.Scanln(&inputGenre) // kita parsing dalu data yang di input dari string ke integers
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	if inputGenre < 1 || inputGenre > len(film.Genres) { // Kita buat validasi, kalau user input 0 itu tidak bisa, ///karena index sekarang dihitung dari 1
-		fmt.Println("Nomor tidak valid") // kita hitung len (film.genders) lalu kita bandingkan dgn data yng di input, Intinya tidak boleh melebihi nilai Len
-		return
-	}
-
-	var newGenre Genre // ini untuk variabel untuk menampung nilai dari &newGenre
-
-	fmt.Print("Masukkan genre baru: ")
-	_, err = fmt.Scanln(&newGenre)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	inputGenre = inputGenre - 1 // kita kurangi 1 karena di atas di tambahkan 1, biar dia membaca index mulai dari 0 dst
-	film.Genres[inputGenre] = newGenre
-	fmt.Println("Genre berhasil diubah:", newGenre)
 }
+
+// func load() {
+
+// }
+
+// func saveMovie() {
+// 	json, err := json.Marshal([]int{1, 2, 3})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	// [1,2,3]
+// 	print(json)
+
+// 	// 2. Write to file (TODO how to do it?)
+// }
 
 func main() {
 	var myFilm Film
+	namaFile := "film.json"
+
+	// myFilmGenres := myFilm.Genres
+	// fmt.Println(myFilmGenres)
+
 	for {
 		fmt.Println("\nMenu: ")
+		fmt.Println("0. Lihat Data Json ")
 		fmt.Println("1. Buat Film Favorit ")
 		fmt.Println("2. Tampilkan Film ")
 		fmt.Println("3. Update Film ")
@@ -207,16 +239,68 @@ func main() {
 		if inputMenu == "e" {
 			fmt.Println("Menutup Aplikasi!")
 			break
+		} else if inputMenu == "0" {
+
+			jsonData, err := os.ReadFile(namaFile)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			fmt.Println(string(jsonData))
+
+			var myDataJson *Film
+
+			err = json.Unmarshal(jsonData, &myDataJson)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			fmt.Println("\nData Json Yang Telah di parsing :")
+			fmt.Println("Judul     :", myDataJson.Judul)
+			fmt.Println("Rating    :", myDataJson.Rating)
+			fmt.Println("Deskripsi :", myDataJson.Deskripsi)
+			fmt.Println("Studio    :", myDataJson.Studio)
+			fmt.Println("Durasi    :", myDataJson.Durasi)
+			fmt.Println("Genres    :", myDataJson.Genres)
+
 		} else if inputMenu == "1" {
 			InputDataFilm(&myFilm)
+			/*  Referensi:
+			JSON Marshal Struct (Mengubah data ke Json):  https://golang.cafe/blog/golang-json-marshal-example.html
+			Membaca data json : https://www.golangprograms.com/golang-writing-struct-to-json-file.html
+			https://stackoverflow.com/questions/24770403/write-struct-to-json-file-using-struct-fields-not-json-keys
+
+			All In 0ne : https://www.developer.com/languages/json-files-golang/
+			*/
+
+			// Ubah data ke dalam bentuk Json
+			jsonData, err := json.Marshal(myFilm)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+			fmt.Println(string(jsonData)) // kita tambahkan string untuk mengubah data byte ke string
+
+			// Simpan data json dengan nama file "data.json"
+
+			err = os.WriteFile(namaFile, jsonData, 0644)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+			fmt.Println("Berhasil menympan data Json dengan nama File : film.json")
+
 		} else if inputMenu == "2" {
 			DataFilm(&myFilm)
 		} else if inputMenu == "3" {
 			UpdateNewFilm(&myFilm)
 		} else if inputMenu == "4" {
 			myFilm = Film{}
-		} else if inputMenu == "5" {
-			UpdateGenre(&myFilm)
 		}
+		// else if inputMenu == "5" {
+		// 	UpdateGenre(&myFilm)
+		// }
 	}
 }
