ebe5456c6e0bf3844de65f5a064401ff71e769cd
eserranda <eser.randa@gmail.com>
2023-08-29 13:47:03 +0800

    Interface 2

--- PR/eser/interface/interface.go
+++ PR/eser/interface/interface.go
@@ -1,45 +1,45 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Food struct {
-	Name  string
-	Price float32
-}
-
-type Drink struct {
-	Name  string
-	Price float32
-}
-
-// type printer interface {
-// 	printInfo()
+// type Food struct {
+// 	Name  string
+// 	Price float32
 // }
 
-func (d Drink) printInfo() {
-	fmt.Printf("Drink : %s  Price : %.2f\n", d.Name, d.Price)
-}
+// type Drink struct {
+// 	Name  string
+// 	Price float32
+// }
 
-func (b Food) printInfo() {
-	fmt.Printf("Food  : %s Price : %.2f\n", b.Name, b.Price)
-}
+// // type printer interface {
+// // 	printInfo()
+// // }
 
-func main() {
-	nasi_goreng := Food{
-		Name:  "Nasi Goreng Jaksel",
-		Price: 12.000,
-	}
+// func (d Drink) printInfo() {
+// 	fmt.Printf("Drink : %s  Price : %.2f\n", d.Name, d.Price)
+// }
 
-	kopi := Drink{
-		Name:  "Kopi Gula Aren",
-		Price: 14.000,
-	}
+// func (b Food) printInfo() {
+// 	fmt.Printf("Food  : %s Price : %.2f\n", b.Name, b.Price)
+// }
 
-	// nasi_goreng.printInfo()
-	// kopi.printInfo()
+// func main() {
+// 	nasi_goreng := Food{
+// 		Name:  "Nasi Goreng Jaksel",
+// 		Price: 12.000,
+// 	}
 
-	info := []printer{nasi_goreng, kopi}
-	for _, item := range info {
-		item.printInfo()
-	}
-}
+// 	kopi := Drink{
+// 		Name:  "Kopi Gula Aren",
+// 		Price: 14.000,
+// 	}
+
+// 	// nasi_goreng.printInfo()
+// 	// kopi.printInfo()
+
+// 	info := []printer{nasi_goreng, kopi}
+// 	for _, item := range info {
+// 		item.printInfo()
+// 	}
+// }
--- PR/eser/interface/interface2.go
+++ PR/eser/interface/interface2.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type printer interface {
+type Print interface {
 	printInfo()
 }
 
@@ -24,21 +24,21 @@ func (d Drink) printInfo() {
 	fmt.Printf("Drink : %s  Price : %.2f\n", d.Name, d.Price)
 }
 
-func createFood(name string, price float32) printer {
+func Makanan(name string, price float32) Print {
 	return Food{Name: name, Price: price}
 }
 
-func createDrink(name string, price float32) printer {
+func Minuman(name string, price float32) Print {
 	return Drink{Name: name, Price: price}
 }
 
 func main() {
-	food1 := createFood("Nasi Goreng", 12.000)
-	food2 := createFood("Mie Ayam", 10.000)
-	drink1 := createDrink("Kopi", 14.000)
-	drink2 := createDrink("Teh", 8.000)
+	food1 := Makanan("Nasi Goreng", 12.000)
+	food2 := Makanan("Mie Ayam", 10.000)
+	drink1 := Minuman("Kopi", 14.000)
+	drink2 := Minuman("Teh", 8.000)
 
-	info := []printer{food1, food2, drink1, drink2}
+	info := []Print{food1, food2, drink1, drink2}
 
 	for _, item := range info {
 		item.printInfo()
