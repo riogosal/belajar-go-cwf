822be732c6576ba642572977b2374760c2cc65d2
eserranda <eser.randa@gmail.com>
2023-08-29 12:46:08 +0800

    Interface

---
+++ PR/eser/ginTest/services/user.service.impl.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"context"
+	"gin-test/models"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserServiceImpl struct {
+	usercollection *mongo.Collection
+	ctx            context.Context
+}
+
+func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserService {
+	return &UserServiceImpl{
+		usercollection: usercollection,
+		ctx:            ctx,
+	}
+}
+
+func (u *UserServiceImpl) CreateUser(user *models.User) error {
+	return nil
+}
+
+func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
+	return nil, nil
+}
+
+func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
+	return nil, nil
+}
+
+func (u *UserServiceImpl) DeleteUser(name *string) error {
+	return nil
+}
+
+func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	return nil
+}
